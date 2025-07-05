d21a14e68e661586410d7907c2724e177ab1337c
KarolKM <karolmackowiak3@gmail.com>
2025-07-05 13:35:58 +0200

    Initial commit - Angular notes app

---
+++ src/app/components/tag-filter/tag-filter.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tag-filter',
+  imports: [],
+  templateUrl: './tag-filter.html',
+  styleUrl: './tag-filter.css'
+})
+export class TagFilter {
+
+}
