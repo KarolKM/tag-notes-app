49e95c309d37e5efbc36f1dea4205cbcb237e3dd
KarolKM <karolmackowiak3@gmail.com>
2025-07-05 19:16:46 +0200

    update

--- src/app/components/tag-filter/tag-filter.ts
+++ src/app/components/tag-filter/tag-filter.ts
@@ -1,12 +1,22 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tag-filter',
+  standalone: true,
   templateUrl: './tag-filter.html',
   styleUrls: ['./tag-filter.scss'],
-  standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatChipsModule,
+    MatButtonModule
+  ],
 })
+
 export class TagFilterComponent {
   @Input() allTags: string[] = [];
   @Output() tagSelected = new EventEmitter<string>();
